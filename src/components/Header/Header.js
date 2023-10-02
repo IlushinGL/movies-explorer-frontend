e4d5481b4e3b4c30a5c507eaa7f1f7a56b5d4c62
IlushinGL <ilushingl@yandex.ru>
2023-10-02 03:57:17 +0300

    refactor: представить CSS Header по БЭМ

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -5,14 +5,14 @@ import './Header.css';
 
 function Header({mediaNum, isAuthorized, linkMain, linkMovies, linkSavedMovies, linkProfile, linkSignUp, onSignInClick, onMenuClick}) {
   const base         = 'header';
-  const linkBase     = `${base}__link`;
+  const linkBase     = `${base}-link`;
   const baseClass    = `${base}`;
-  const navClass     = `${base}__nav`;
-  const authBtnClass = `${base}__auth-btn ${base}__auth-btn_pos_${mediaNum}`;
+  const navClass     = `${base}-nav`;
+  const authBtnClass = `${base}-auth-btn ${base}-auth-btn_pos_${mediaNum}`;
   const authNavClass = `${linkBase}-auth ${linkBase}-auth_pos_${mediaNum}`;
-  const logoClass    = `${base}__logo ${base}__logo_pos_${mediaNum}`;
-  const menuClass    = `${base}__menu ${base}__menu_pos_${mediaNum}`;
-  const btnClass     = `${base}__profile-btn`;
+  const logoClass    = `${base}-logo ${base}-logo_pos_${mediaNum}`;
+  const menuClass    = `${base}-menu ${base}-menu_pos_${mediaNum}`;
+  const btnClass     = `${base}-profile-btn`;
   if (isAuthorized) {
 
     return (
